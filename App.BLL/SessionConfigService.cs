7f0bb9d9a9b4641d45e9a9900d0b2389e175c472
ethel <ethupp@taltech.ee>
2026-06-04 18:30:34 +0300

    Added webapp project

---
+++ App.BLL/SessionConfigService.cs
@@ -0,0 +1,41 @@
+﻿namespace App.BLL;
+
+public class SessionConfigService
+{
+    public void ValidateSessionConfig(SessionConfig config)
+    {
+        var anchors = config.SessionConfigChips
+            .Where(x => x.Role == ChipRole.Anchor)
+            .ToList();
+
+        var tags = config.SessionConfigChips
+            .Where(x => x.Role == ChipRole.Tag)
+            .ToList();
+
+        if (anchors.Count < 3)
+        {
+            throw new Exception("A session configuration must have at least 3 anchors.");
+        }
+
+        if (tags.Count < 1)
+        {
+            throw new Exception("A session configuration must have at least 1 tag.");
+        }
+
+        foreach (var anchor in anchors)
+        {
+            if (anchor.XCoord == null || anchor.YCoord == null)
+            {
+                throw new Exception("Anchor chips must have coordinates.");
+            }
+        }
+
+        foreach (var tag in tags)
+        {
+            if (tag.XCoord != null || tag.YCoord != null || tag.ZCoord != null)
+            {
+                throw new Exception("Tag chips should not have fixed coordinates.");
+            }
+        }
+    }
+}
---
+++ App.DAL.EF/AppDbContext.cs
@@ -0,0 +1,70 @@
+﻿namespace App.DAL.EF;
+
+public class AppDbContext : DbContext
+{
+public DbSet<Chip> Chips { get; set; } = default!;
+    public DbSet<SessionConfig> SessionConfigs { get; set; } = default!;
+    public DbSet<SessionConfigChip> SessionConfigChips { get; set; } = default!;
+    public DbSet<Session> Sessions { get; set; } = default!;
+    public DbSet<PositionResult> PositionResults { get; set; } = default!;
+    public DbSet<RawMeasurement> RawMeasurements { get; set; } = default!;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Chip>()
+            .HasIndex(x => x.DeviceIdentifier)
+            .IsUnique();
+
+        builder.Entity<SessionConfigChip>()
+            .HasIndex(x => new { x.SessionConfigId, x.ChipId })
+            .IsUnique();
+
+        builder.Entity<SessionConfigChip>()
+            .HasOne(x => x.SessionConfig)
+            .WithMany(x => x.SessionConfigChips)
+            .HasForeignKey(x => x.SessionConfigId);
+
+        builder.Entity<SessionConfigChip>()
+            .HasOne(x => x.Chip)
+            .WithMany(x => x.SessionConfigChips)
+            .HasForeignKey(x => x.ChipId);
+
+        builder.Entity<Session>()
+            .HasOne(x => x.SessionConfig)
+            .WithMany(x => x.Sessions)
+            .HasForeignKey(x => x.SessionConfigId);
+
+        builder.Entity<PositionResult>()
+            .HasOne(x => x.Session)
+            .WithMany(x => x.PositionResults)
+            .HasForeignKey(x => x.SessionId);
+
+        builder.Entity<PositionResult>()
+            .HasOne(x => x.TagChip)
+            .WithMany(x => x.PositionResultsAsTag)
+            .HasForeignKey(x => x.TagChipId);
+
+        builder.Entity<RawMeasurement>()
+            .HasOne(x => x.Session)
+            .WithMany(x => x.RawMeasurements)
+            .HasForeignKey(x => x.SessionId);
+
+        builder.Entity<RawMeasurement>()
+            .HasOne(x => x.TagChip)
+            .WithMany(x => x.RawMeasurementsAsTag)
+            .HasForeignKey(x => x.TagChipId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Entity<RawMeasurement>()
+            .HasOne(x => x.AnchorChip)
+            .WithMany(x => x.RawMeasurementsAsAnchor)
+            .HasForeignKey(x => x.AnchorChipId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
